398d13f31241b7e1cac67bd293c95d33d247685f
bolonge <qhsghd5357@gamil.com>
2021-12-28 18:01:21 +0900

    feed 주석

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,10 @@
- $(document).ready(function () {
+
+
+$(document).ready(function () {
 
         });
 
-
+        //feed 는 메인 화면에 들어갈 post를 받아서 뿌리는 역할입니다
          function feed() {
             $.ajax({
                 type: 'GET',
@@ -12,4 +14,6 @@
 
                 }
             })
+
+             //여기다가 포스트 html 을 foreach
         }
