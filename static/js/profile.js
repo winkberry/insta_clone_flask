06d20dcc99444e1e67d3357beb33112022ab946f
seunghwan13 <childwelfare23@gmail.com>
2021-12-29 15:56:14 +0900

    메인 화면으로 돌아가는 버튼 재구현

--- static/js/profile.js
+++ static/js/profile.js
@@ -13,7 +13,9 @@ $(document).ready(function () {
                 }
             })
         }
-/*메인페이지로 돌아가는 함수*/
-function to_main() {
-    window.location.href = "/"
-}
+
+        /*메인페이지로 돌아가는 함수*/
+
+        function to_main() {
+            window.location.href = "/"
+        }
