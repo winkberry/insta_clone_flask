95967d1666bdcb55edd78bf1ed3ba7b29e824ce8
sahayana <saokhueyang@gmail.com>
2021-12-30 12:18:58 +0900

    로그인 및 회원가입 기능 추가

---
+++ static/js/login.js
@@ -0,0 +1,22 @@
+function login() {
+    let data = {
+        id : $("#id").val(),
+        pw : $("#pw").val()
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: '/login',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(data),
+        success: function (response) {
+            if (response["result"] == "success") {
+                $.cookie('token', response['token']);
+                alert('로그인 하였습니다.');
+                window.location.href = '/';
+            } else {
+                alert(response['msg']);
+            }                    
+        }
+    })
+}
