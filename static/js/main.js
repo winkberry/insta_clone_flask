393bb8a04fb901f81947282292b8f9e255333a34
bolonge <qhsghd5357@gamil.com>
2021-12-30 16:48:04 +0900

    posting 부분 수정 app.py 파일전송부분 삭제

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,26 @@
-    $(document).ready(function () {
+$(document).ready(function () {
     get_feed();
 });
-        //feed 는 메인 화면에 들어갈 post를 받아서 뿌리는 역할입니다
-         function get_feed() {
-            $.ajax({
-                type: 'GET',
-                url: '/api/feed',
-                data: {},
-                success: function (response) {
-                    let row = JSON.parse(response)
 
-                }
-            })
-             //여기다가 포스트 html 을 foreach
-        }
+//feed 는 메인 화면에 들어갈 post를 받아서 뿌리는 역할입니다
+function get_feed() {
+    $.ajax({
+        type: 'GET',
+        url: '/api/feed',
+        data: {},
+        success: function (response) {
+            let row = JSON.parse(response)
 
-        //로그인한 유저의 이름을 보내준다
-        function go_profile(){
-             window.location.href=`/profile?username=${'123'}` //일단은 123으로 보내준다
         }
+    })
+    //여기다가 포스트 html 을 foreach
+}
+
+//로그인한 유저의 이름을 보내준다
+function go_profile() {
+    window.location.href = `/profile?username=${'123'}` //일단은 123으로 보내준다
+}
+
+function go_posting(id) {
+    window.location.href = `/posting?id=${id}`
+}
